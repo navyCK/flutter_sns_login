8c5350eb8e5359f510bdbf6d1f52e680eed546d4
navyCK <ckck81026@gmail.com>
2021-10-06 14:10:40 +0900

    [SL-06] 로딩 화면 테스트

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -54,6 +54,37 @@ class _LoginScreenState extends State<LoginScreen> {
     FirebaseAuth.instance.signInAnonymously();
   }
 
+  void _loading() {
+    FutureBuilder(
+        future: signInWithGoogle(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          //해당 부분은 data를 아직 받아 오지 못했을때 실행되는 부분을 의미한다.
+          if (snapshot.hasData == false) {
+            return CircularProgressIndicator();
+          }
+          //error가 발생하게 될 경우 반환하게 되는 부분
+          else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: TextStyle(fontSize: 15),
+              ),
+            );
+          }
+          // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
+          else {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                snapshot.data.toString(),
+                style: TextStyle(fontSize: 15),
+              ),
+            );
+          }
+        });
+  }
+
   Future<UserCredential> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -160,7 +191,7 @@ class _LoginScreenState extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               buildElevatedButton(
-                  signInWithGoogle, "assets/icon/icon_google.png", "구글 로그인", Color(0xffffffff), Colors.black54
+                  _loading, "assets/icon/icon_google.png", "구글 로그인", Color(0xffffffff), Colors.black54
               ),
 
               buildElevatedButton(
